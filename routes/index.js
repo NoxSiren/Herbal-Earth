f59e834c26ed52c413c0b8935138d4e76c7c3d11
David Gavino <ggavino2@uccs.edu>
2022-02-15 19:02:25 -0700

    modified files

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const apiRoutes = require('./apiRoutes');
-const homeRoutes = require('./homeRoutes');
-
-router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
-
-module.exports= router;
