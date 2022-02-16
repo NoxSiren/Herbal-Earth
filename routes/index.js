3b9d61b9bceea3bfc087f43d9b7704115377eb6c
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-02-15 20:31:17 -0700

    working on routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const apiRoutes= require('./apiRoutes');
 const {renderHomePage, getAllRecipes, getAllDrinks, getAllFood}= require('../controllers/homePageController')
-const {renderHomePage,loginView, signupView} = require('../controllers/userController');
+const {loginView, signupView} = require('../controllers/userController');
 const {getUserRecipes}= require('../controllers/dashboardController');
 
 router.get('/', renderHomePage, getAllRecipes);
@@ -9,7 +9,7 @@ router.get('/drinks', getAllDrinks);
 router.get('/food', getAllFood);
 router.get('/dashboard', getUserRecipes);
 router.get('/login', loginView);
-router.get('signup', signupView);
+router.get('/signup', signupView);
 router.get('/api', apiRoutes);
 
 module.exports = router;
