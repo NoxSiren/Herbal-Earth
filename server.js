e249af5e6b7a37958eb278b2b60b288f9d83ca0b
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-02-17 18:25:14 -0700

    updating code

--- server.js
+++ server.js
@@ -3,19 +3,18 @@ require('dotenv').config();
 const path = require ('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-// const exphbs = require('express-session');
-// const session = require('express-session');
+const session = require('express-session');
 const routes = require('./routes');
 const sequelize = require('./config');
 
 //Creating Port connection
 const app = express();
 const PORT = process.env.PORT || 3001; 
-// const sessionSettings = {
-//     secret: process.env.SESSION_SECRET,
-//     resave: false, 
-//     saveUninitialized: false,
-// };
+const sessionSettings = {
+    secret: process.env.SESSION_SECRET,
+    resave: false, 
+    saveUninitialized: false,
+};
 const hbs = exphbs.create();
 
 //Initialize view engine
@@ -26,7 +25,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(session(sessionSettings));
+app.use(session(sessionSettings));
 app.use(routes);
 
 sequelize.sync({force: true}).then(()=>{
