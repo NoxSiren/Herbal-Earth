b1448d96a4e42e3f5dbc04d625cd2bf79e5327c6
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-03-08 22:29:48 -0700

    more changes to routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const apiRoutes= require('./apiRoutes');
-const {renderHomePage, getAllRecipes, getAllDrinks, getAllFood}= require('../controllers/homePageController')
+const {renderHomePage, getAllRecipes, getAllDrinks,}= require('../controllers/homepageController')
 const {loginView, signupView} = require('../controllers/userController');
 const {getUserRecipes}= require('../controllers/dashboardController');
 
 router.get('/', renderHomePage, getAllRecipes);
 router.get('/drinks', getAllDrinks);
-router.get('/food', getAllFood);
+// router.get('/food', getAllFood);
 router.get('/dashboard', getUserRecipes);
 router.get('/signin', loginView);
 router.get('/signup', signupView);
