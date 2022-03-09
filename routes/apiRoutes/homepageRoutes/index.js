6c2abc138dcb81a07b6f91fbbb516e69ca504940
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-03-08 22:22:22 -0700

    changes to routes

--- routes/apiRoutes/homepageRoutes/index.js
+++ routes/apiRoutes/homepageRoutes/index.js
@@ -1,17 +1,17 @@
 const router = require('express').Router();
 
 const {
-    createUser,
-    login,
+    // createUser,
+    // login,
     signupHandler,
     logout,
 }= require ('../../../controllers/homepageController');
 
-router.route('/')
-    .post(createUser);
+// router.route('/')
+//     .post(createUser);
 
 router.post('/signup', signupHandler);
-router.post('/login', login);
+// router.post('/login', login);
 router.post('/logout', logout);
 
 module.exports= router;
