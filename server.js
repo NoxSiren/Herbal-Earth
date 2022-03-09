5255da975d668b6e2e805ce29cbb5644dd802875
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-03-08 21:45:57 -0700

    created procfile for heroku

--- server.js
+++ server.js
@@ -3,27 +3,17 @@ require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-<<<<<<< HEAD
-=======
 // const exphbs = require('express-session');
->>>>>>> 4aa22910abad326124e11dd2a432797ac02ed319
 const session = require('express-session');
 const routes = require('./routes');
 const sequelize = require('./config');
 
 //Creating Port connection
 const app = express();
-<<<<<<< HEAD
-const PORT = process.env.PORT || 3001; 
-const sessionSettings = {
-    secret: process.env.SESSION_SECRET,
-    resave: false, 
-=======
 const PORT = process.env.PORT || 3001;
 const sessionSettings = {
     secret: process.env.SESSION_SECRET,
     resave: false,
->>>>>>> 4aa22910abad326124e11dd2a432797ac02ed319
     saveUninitialized: false,
 };
 const hbs = exphbs.create();
@@ -34,13 +24,8 @@ app.set('view engine', 'handlebars');
 
 
 app.use(express.json());
-<<<<<<< HEAD
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'public')));
-=======
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
->>>>>>> 4aa22910abad326124e11dd2a432797ac02ed319
 app.use(session(sessionSettings));
 app.use(routes);
 
